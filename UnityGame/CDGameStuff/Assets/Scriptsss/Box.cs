886cb186fb93bc8d0a5ee34130889b3b04e928e4
Unknown <stheadman1@hotmail.com>
2018-02-28 21:09:40 -0700

    Created Scriptable objects in unity

---
+++ UnityGame/CDGameStuff/Assets/Scriptsss/Box.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Box", menuName = "Box")]
+public class Box : ScriptableObject {
+
+    public Vector3 Size;
+    public new string name;
+
+    public Color color;
+
+    public int health;
+    public int power;
+
+    public void Print()
+    {
+        Debug.Log(name + " Health: " + health + " " + "Power: " + power);
+    }
+
+}
---
+++ UnityGame/CDGameStuff/Assets/Scriptsss/BoxDesplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDesplay : MonoBehaviour {
+
+    public Box box;
+
+    public Transform boxSize;
+
+    public Color boxColor;
+    public Renderer boxRend;
+
+	void Start () {
+        box.Print();
+        boxRend.material.color = box.color;
+        boxSize.localScale = box.Size;
+	}
+
+}
